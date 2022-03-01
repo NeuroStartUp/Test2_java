f23916140aba65466139a519996a2da43085782c
Duinova <Ganenok80@mail.ru>
2022-03-01 18:36:23 +0700

    Доработка ДЗ

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int ticketAmount = 150; // Стоимость билета
         int numberOfRubles = 20; // Количество рублей за 1 милю
         int bonus = ticketAmount / numberOfRubles; // Количество бонусных миль
-        System.out.println(" Всего бонусных миль:" + (bonus));
+
+        System.out.println(" Всего бонусных миль: " + (bonus));
     }
 }
